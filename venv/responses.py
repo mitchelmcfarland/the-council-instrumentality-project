cec341a0f60eeb469532b617aca613eb29840c61
mitchelmcfarland <mitchymoo1203@gmail.com>
2024-09-25 10:43:20 -0400

    not working

--- venv/responses.py
+++ venv/responses.py
@@ -8,10 +8,20 @@ client = Groq(
     api_key=os.getenv("GROQ_API_KEY"),
 )
 
+
+
+with open('venv/all_messages.txt', 'r', encoding='utf-8') as file:
+    file_content = file.read()
+
 def get_response(user_input: str) -> str:
     try:
         chat_completion = client.chat.completions.create(
             messages=[
+                {
+                    "role": "system",
+                    "content": "You will play a character known as the Council Instrumentality Project. This is the amalgamation of several characters, and you are to produce a response that is most representative of this collective personality. The characters you are emulating are as follows:" + file_content + "\nRespond to all messages as the Council Instrumentality Project",
+                },
+                
                 {
                     "role": "user",
                     "content": user_input,
