3e2ad8cb516529101ed78d08d1227cc13f13b142
mitchelmcfarland <143099917+mitchelmcfarland@users.noreply.github.com>
2024-09-24 20:43:35 -0400

    implemented responses mock

--- venv/main.py
+++ venv/main.py
@@ -7,3 +7,26 @@ from responses import get_response
 #load token
 load_dotenv()
 TOKEN: Final[str] = os.getenv('DISCORD_TOKEN')
+
+#bot setup
+intents: Intents = Intents.default()
+intents.message_content = True
+client: Client = Client(intents=intents)
+
+#message functionality
+async def send_message(message: Message, user_message: str) -> None:
+    if not user_message:
+        print('(intents dont work fuck)')
+        return
+    
+    if is_private := user_message[0] == '?':
+        user_message = user_message[1:]
+        
+    try:
+        response: str = get_response(user_message)
+        await message.author.send(response) if is_private else message.channel.send(response)
+    except Exception as e:
+        print(e)
+        
+    
+    
--- venv/responses.py
+++ venv/responses.py
@@ -1,2 +1,14 @@
+from random import choice, randint
+
 def get_response(user_input: str) -> str:
-    raise NotImplemented('not done')
+    lowered:str = user_input.lower()
+    
+    
+    if lowered == '':
+        return 'Speak up bruh'
+    elif 'hello' in lowered:
+        return 'wsg gang'
+    elif 'roll dice' in lowered:
+        return f'thas a {randint(1, 6)}'
+    else:
+        return 'im a fraud :('
